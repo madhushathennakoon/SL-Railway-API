62779619eec666c34e4ef56e75f634943a556960
madhushathennakoon <madhushasampath@gmail.com>
2024-08-27 23:46:49 +0530

    create train routes and controllers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 
 // Express app
 const app = express();
+
+// Body parser, reading data from body into req.body
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/train");
 
 module.exports = app;
